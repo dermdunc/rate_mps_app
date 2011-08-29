e4658fcd93433e636af5d81c0ada92bda2205060
Derm <dermduncan@gmail.com>
2011-08-29 13:16:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMpsApp::Application.config.secret_token = '062b2218dc9da52a5fcf840846021b706fe3d45942b5877113a3a832a668be528a76532acee17428058dd8dd334f155c0c11256294cc4b7b84f1f61e15ea3493'
